69cab2979f8f64a8a54020d9c9a0dccf3601bb08
Berchem <34000467+Berchem@users.noreply.github.com>
2018-08-11 01:35:53 +0800

    v0.1.2

--- MySQL/build_simple_db.sql
+++ MySQL/build_simple_db.sql
@@ -1,14 +1,33 @@
 # first log into MySQL, create a database
-create database practice;
+create database scraping;
 
 # specify a database to interact
-use practice;
+use scraping;
 
 # create a table. a table in MySQL can't exist without columns
-create table test(
-	id int(4) not null auto_increment,
-    lable1 varchar(20),
-    usr_name varchar(50),
-    log timestamp default current_timestamp,
+create table pages(
+	id bigint(7) not null auto_increment,
+    title varchar(200),
+    content varchar(10000),
+    created timestamp default current_timestamp,
     primary key (id)
 );
+
+# drop table
+drop table pages;
+
+# insert some data to table
+insert into pages(title, content) values (
+	"test page title",
+    "this is some test page content. it can be up to 10000 characters longs"
+);
+
+# override default (auto_increment, current_timestamp)
+insert into pages (id, title, content, created) values (
+	3,
+    "test page title",
+    "content",
+    "1991-01-18 11:46:30"
+);
+
+select * from pages where id = 2;
